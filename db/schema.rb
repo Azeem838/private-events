89a11dc0e8f0f40034cf1024ed4869f3ebc7fc33
Azeem <azeemahmed101@gmail.com>
2020-06-19 14:10:35 +0200

    add events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_19_102620) do
+ActiveRecord::Schema.define(version: 2020_06_19_104520) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "location"
+    t.datetime "event_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
