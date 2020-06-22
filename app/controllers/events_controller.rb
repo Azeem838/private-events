d56eb2eb741d7daccfb732f2c7d94c59f4a9bf0d
rajkumardas2701 <rajkumardas2701@gmail.com>
2020-06-22 20:34:17 +0530

    added show page for events created

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,11 @@
+class EventsController < ApplicationController
+	def new
+	end
+
+	def index
+	end
+
+	def show
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'users/:id', to: 'users#show'
+  resources :events, only: [:new, :index, :show]
+
   root to: "home#index"
 end
