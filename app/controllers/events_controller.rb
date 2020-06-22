9cbb47d91a9c9262c92e6c136b08c9eda328a522
rajkumardas2701 <rajkumardas2701@gmail.com>
2020-06-22 21:31:20 +0530

    displaying creator of the event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,36 @@
 class EventsController < ApplicationController
+	
 	def new
+		@event = Event.new
 	end
 
 	def index
 	end
 
 	def show
+		@event = Event.find(params[:id])
+		@user = User.find(@event.creator_id)
+	end
+
+	def create
+		@event = current_user.events.build(events_params)
+		# @event = Event.new(params[:id])
+
+  #   respond_to do |format|
+  #     if @blog.save
+  #       format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
+       
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @blog.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+	end
+
+	private 
+
+	def event_params
+		require(:events).permit(:location, :event_date)
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'users/:id', to: 'users#show'
-  resources :events, only: [:new, :index, :show]
+  resources :events, only: [:new, :index, :show, :create]
 
   root to: "home#index"
 end
