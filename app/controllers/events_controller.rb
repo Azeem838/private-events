1d9de546e29da2bd436830bc3c27489c816cb133
Azeem <azeemahmed101@gmail.com>
2020-06-23 12:27:56 +0200

    add upcoming and previous events to index events page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,37 +1,52 @@
 class EventsController < ApplicationController
-	
-	def new
-		@event = Event.new
-	end
+	helper_method :upcoming, :past 
+  def new
+    @event = Event.new
+  end
 
-	def index
-		@events = Event.all
-	end
+  def index
+    @events = Event.all
+  end
 
-	def show
-		@event = Event.find(params[:id])
-		@user = User.find(@event.creator_id)
-	end
+  def shows
+    @event = Event.find(params[:id])
+    @user = User.find(@event.creator_id)
+  end
 
-	def create
-		@event = current_user.events.build(events_params)
-		# @event = Event.new(params[:id])
+  def past
+    past_events = []
+    Event.all.each do |event|
+      past_events << event if event.event_date < Time.now
+    end
+    past_events
+  end
 
-  #   respond_to do |format|
-  #     if @blog.save
-  #       format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
-       
-  #     else
-  #       format.html { render :new }
-  #       format.json { render json: @blog.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-	end
+  def upcoming
+    upcoming_events = []
+    Event.all.each do |event|
+      upcoming_events << event if event.event_date >= Time.now
+    end
+    upcoming_events
+  end
 
-	private 
+  def create
+    @event = current_user.events.build(events_params)
+    # @event = Event.new(params[:id])
 
-	def event_params
-		require(:events).permit(:location, :event_date)
-	end
+    #   respond_to do |format|
+    #     if @blog.save
+    #       format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
 
+    #     else
+    #       format.html { render :new }
+    #       format.json { render json: @blog.errors, status: :unprocessable_entity }
+    #     end
+    #   end
+  end
+
+  private
+
+  def event_params
+    require(:events).permit(:location, :event_date)
+  end
 end
