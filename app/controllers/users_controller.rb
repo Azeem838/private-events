81fb92d95b274344a71e7ed10d8ba6ccd8b6b56f
Azeem <azeemahmed101@gmail.com>
2020-06-23 12:12:50 +0200

    add previous and upcoming events to users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 class UsersController < ApplicationController
+  def index
+    @user = User.all
+  end
 
-	def index
-		@user = User.all
-	end
-
-	def show
-		@user = User.find(params[:id])
-	end
-
+  def show
+    @user = User.find(params[:id])
+    @events = @user.events + @user.attended_events
+    @upcoming_events = []
+    @previous_events = []
+    @events.each do |event|
+      if Time.now < event.event_date
+        @upcoming_events << event
+      else
+        @previous_events << event
+      end
+    end
+  end
 end
