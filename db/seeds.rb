e635e1afd7566850f113cec37a912a3f460ac8ad
Azeem <azeemahmed101@gmail.com>
2020-06-23 17:43:53 +0200

    add show with list of attendees on an event

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Invite.create(user_id: 1, event_id: 7)
+Invite.create(user_id: 2, event_id: 7)
+Invite.create(user_id: 3, event_id: 7)
+Invite.create(user_id: 4, event_id: 7)
+Invite.create(user_id: 5, event_id: 7)
+
+Invite.create(user_id: 1, event_id: 8)
+Invite.create(user_id: 2, event_id: 8)
+Invite.create(user_id: 3, event_id: 8)
+Invite.create(user_id: 4, event_id: 8)
+Invite.create(user_id: 5, event_id: 8)
