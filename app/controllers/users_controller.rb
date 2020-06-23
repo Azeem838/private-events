f94d35a9d767dbb273a4df2b31a9e2e6288bfd25
Azeem <azeemahmed101@gmail.com>
2020-06-23 18:45:44 +0200

    add separated events for users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @events = @user.events + @user.attended_events
-    @upcoming_events = Event.upcoming
-    @previous_events = Event.past
+    @upcoming_events = Event.upcoming.where(creator_id: @user.id)
+    @previous_events = Event.past.where(creator_id: @user.id)
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,6 @@ class Event < ApplicationRecord
   has_many :attendees, through: :invites, source: :user
   scope :upcoming, -> { where('event_date > ?', Time.now) }
   scope :past, -> { where('event_date < ?', Time.now) }
+  validates :location, presence: true
+  validates :event_date, presence: true
 end
