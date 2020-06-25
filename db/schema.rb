471a85dcd94d53765ba2e376a0eaee93fcf15a1b
Azeem <azeemahmed101@gmail.com>
2020-06-25 19:41:48 +0200

    add index user_id and event_id to index table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_080830) do
+ActiveRecord::Schema.define(version: 2020_06_25_173905) do
 
   create_table "events", force: :cascade do |t|
     t.string "location"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2020_06_24_080830) do
     t.integer "event_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_invites_on_event_id"
+    t.index ["user_id"], name: "index_invites_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
